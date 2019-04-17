d287a416ba22af1ddcf31cbaa27dfcb1f4ca6d66
lara <lara.ferrer001@gmail.com>
2019-04-17 11:46:27 -0300

    arraylist resolvido

--- src/framework/Desktop.java
+++ src/framework/Desktop.java
@@ -8,11 +8,11 @@ public class Desktop extends Dispositivos implements Proporcao{
        double l, p, lis;
         for (int i = 0; i < this.getLink().size(); i++) {
             l = this.getLink().get(i).getTamanho()*1.12;
-            this.getLink().get(i).setTamanho(l);
+            this.getLink().get(i).setTamanho(l+this.getLink().get(i).getTamanho());
             p = this.getParag().get(i).getTamanho()*1.15;
-            this.getParag().get(i).setTamanho(p);
+            this.getParag().get(i).setTamanho(p+this.getParag().get(i).getTamanho());
             lis = this.getLista().get(i).getTamanho()*1.15;
-            this.getLista().get(i).setTamanho(lis);
+            this.getLista().get(i).setTamanho(lis+this.getLista().get(i).getTamanho());
         }
         
     }
--- src/framework/Notebook.java
+++ src/framework/Notebook.java
@@ -9,11 +9,11 @@ public class Notebook extends Dispositivos implements Proporcao{
         double l, p, lis;
         for (int i = 0; i < this.getLink().size(); i++) {
             l = this.getLink().get(i).getTamanho()*2.0;
-            this.getLink().get(i).setTamanho(l);
+            this.getLink().get(i).setTamanho(l+this.getLink().get(i).getTamanho());
             p = this.getParag().get(i).getTamanho()*2.0;
-            this.getParag().get(i).setTamanho(p);
+            this.getParag().get(i).setTamanho(p+this.getParag().get(i).getTamanho());
             lis = this.getLista().get(i).getTamanho()*2.0;
-            this.getLista().get(i).setTamanho(lis);
+            this.getLista().get(i).setTamanho(lis+this.getLista().get(i).getTamanho());
         }
        
     }
